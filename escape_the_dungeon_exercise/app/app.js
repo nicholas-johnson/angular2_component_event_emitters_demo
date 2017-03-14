b4e8cebc4aa2e43bcfe6b6b1dd50f3e85b8f058e
Nicholas Johnson <nicholas@forwardadvance.com>
2017-03-14 16:05:20 +0000

    removeItem

--- escape_the_dungeon_exercise/app/app.js
+++ escape_the_dungeon_exercise/app/app.js
@@ -21,6 +21,9 @@ var locationModel = {
   name: "Nondescript Corridor",
   description: "It is very dark. To the north you can just make out a faint glimmer of golden light.",
   exits: {north:true},
+  removeItem: function(item) {
+    this.items.splice(this.items.indexOf(item), 1)
+  },
   items: [
     {
       name: "rusty_sword",
